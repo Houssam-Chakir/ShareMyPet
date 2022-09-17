5e55cb37b5ffa8b774af68288af67427dd3c955e
yassine ambari <ambari.yassine@gmail.com>
2022-09-17 16:43:20 +0100

    model animals migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_151753) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_154123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "specie"
+    t.integer "age"
+    t.float "price_hour"
+    t.text "bio"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_animals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_151753) do
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "animals", "users"
 end
