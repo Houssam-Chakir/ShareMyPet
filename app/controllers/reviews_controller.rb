41f22e913ae09fc1153eee23eee797e7ab6785b2
Amal LOUDIYI <amal.loudiyi@gmail.com>
2022-09-20 21:40:50 +0100

    reviews controller done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+
+  # GET /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # POST /reviews
+  # POST /reviews.json
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.save
+
+    render :success
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to list_path(@review.list)
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def review_params
+    params.require(:review).permit(:comment, :rating)
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+
 end
