3c0b0ff447d9e746847dfa00d7d72f6dcfdc9c0f
houssam chakir <thisischchannel@gmail.com>
2022-09-20 21:21:18 +0100

    created index show new creare and destroy

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,34 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    @review = Review.new
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @animal.destroy
+    redirect_to animals_path, status: :see_other
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :address, :phone_number, :category)
+  end
 end
