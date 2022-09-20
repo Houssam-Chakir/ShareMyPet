1e93f254ce34f761e5843f172fceccb5e6d2c57b
yassine ambari <ambari.yassine@gmail.com>
2022-09-20 21:33:55 +0100

    controller bookings créé

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,56 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: :destroy
+  before_action :set_animal
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    set_booking
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+    set_booking
+  end
+
+  def update
+    set_booking
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def create
+    @booking = booking.new(booking_params)
+    @booking.animal = @animal
+    if @booking.save
+      redirect_to animal_path(@animal)
+    else
+      @review = Review.new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to animal_path(@booking.animal), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :animal_id, :user_id, :review_id)
+  end
+
+  def set_booking
+    @booking = booking.find(params[:id])
+  end
+
+  def set_animal
+    @animal = Animal.find(params[:animal_id])
+  end
 end
