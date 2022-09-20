6e9c28922388d07911f96e6df832ef1ca73805eb
yassine ambari <ambari.yassine@gmail.com>
2022-09-20 21:04:11 +0100

    routes créées et review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :views
   devise_for :users
-  root to: "pages#home"
+  root to: "animals#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :animals do
+      resources :bookings
+      resources :reviews
+  end
+  resources :bookings, only: :destroy
+  resources :reviews, only: :destroy
 end
