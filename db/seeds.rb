4a78791fef543f8ed74e14f6ee9792c7c2776e4d
houssam chakir <thisischchannel@gmail.com>
2022-09-22 19:20:57 +0100

    created animals view, and navbar render. added faker gem (plz bundle install), and made some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Destroying database
+puts 'destroying data base!'
+# Animal.destroy
+puts 'done'
+
+puts 'creating user 1'
+user = User.create(first_name: 'Houssam', last_name: 'Chakir')
+puts 'done'
+
+puts 'adding animals'
+10.times do
+  animal = Animal.create(
+    name: Faker::Creature::Animal.name,
+    category: Faker::Creature::Dog.breed,
+    specie: Faker::Creature::Dog.meme_phrase,
+    bio: Faker::Creature::Dog.sound,
+    age: Faker::Creature::Dog.age,
+    price_hour: 1)
+  puts "added #{animal.name} to make someone happy"
+end
+puts 'done'
