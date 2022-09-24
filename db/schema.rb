af0e9b3ebedb48588748f6825846dbe2004fb6d9
Jihane El Jbari <eljbari.jihane@gmail.com>
2022-09-24 15:42:49 +0100

    changes on model and geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_201714) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_143713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_201714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "adress"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
