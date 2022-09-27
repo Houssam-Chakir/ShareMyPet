cb7bcfc75ad5ba6cecfc943b4a7c2c759fe56c17
Jihane El Jbari <eljbari.jihane@gmail.com>
2022-09-27 20:59:35 +0100

    mapbox settings added

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,8 +3,8 @@ class Animal < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
-  geocoded_by :adress
-  after_validation :geocode, if: :will_save_change_to_adress?
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   validates :price_hour, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_143713) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_190656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_143713) do
     t.string "image"
     t.float "latitude"
     t.float "longitude"
-    t.text "adress"
+    t.text "address"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
